7ff2bd10f28fdeb8a1dd9ff6734f11374022782d
KpucKoT <167256393+KpucKoT@users.noreply.github.com>
2024-07-22 22:50:08 +0300

    Add files via upload

---
+++ PP_2_1_3_SpringBeans/src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,35 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal;
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    private Timer timer;
+
+    @Autowired
+    public AnimalsCage(Timer timer) {
+        this.timer = timer;
+    }
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(timer.getTime());
+        System.out.println("________________________");
+    }
+
+
+
+}
